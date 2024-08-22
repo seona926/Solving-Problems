8023b7cd0c888848556c1e540f83dcf8fdd537d0
Sophia <60469440+seona926@users.noreply.github.com>
2024-08-22 14:39:08 +0900

    Time: 82 ms (37.10%), Space: 58.1 MB (28.47%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.js
+++ 0338-counting-bits/0338-counting-bits.js
@@ -3,21 +3,17 @@
  * @return {number[]}
  */
 let countBits = function(n) {
-    let arr = [];
     let result = [];
     
     for (let i = 0; i <= n; i++) {
-        let count = 0;
         let binaryString = i.toString(2);
         
+        let count = 0;
         for (let item of binaryString) {
-            if (item % 2 === 1) {
+            if (item === '1') {
               count++;
             }
-
-            binaryString = Math.floor(binaryString / 2);
         }
-        
         
         result.push(count);
     }
